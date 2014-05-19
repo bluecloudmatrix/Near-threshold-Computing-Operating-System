d7497e025e2d2105fecdbe963b223afc8a0180be
bluecloudmatrix <bluecloudmatrix@gmail.com>
2014-05-19 22:34:00 +0800

    1

--- tolset/harib01b/bootpack.c
+++ tolset/harib01b/bootpack.c
@@ -4,7 +4,7 @@ void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
-	int i; /* 変数宣言。iという変数は、32ビットの整数型 */
+	int i; /* i is an 32-bit integer */
 
 	for (i = 0xa0000; i <= 0xaffff; i++) {
 		write_mem8(i, i & 0x0f);
