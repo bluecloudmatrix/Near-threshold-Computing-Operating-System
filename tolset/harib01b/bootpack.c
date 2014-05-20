008d8f7200bacfe03bf65f4e704c2812a589c5ee
bluecloudmatrix <bluecloudmatrix@gmail.com>
2014-05-21 01:12:25 +0800

    gdtidt_harib02i

--- tolset/harib01b/bootpack.c
+++ tolset/harib01b/bootpack.c
@@ -42,6 +42,25 @@ struct BOOTINFO {
 	char *vram;
 };
 
+/* GDT/IDT */
+
+struct SEGMENT_DESCRIPTOR {
+	short limit_low, base_low;
+	char base_mid, access_right;
+	char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR {
+	short offset_low, selector;
+	char dw_count, access_right;
+	short offset_high;
+};
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+void init_gdtidt(void);
+
+
 void HariMain(void)
 {
 	//int i; /* i is an 32-bit integer */
@@ -60,6 +79,8 @@ void HariMain(void)
 		0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
 		0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00
 	};*/
+	
+	//init_gdtidt();
 	
 	init_palette(); /* setting palette */
 	
@@ -272,3 +293,51 @@ void putblock8_8(char *vram, int vxsize, int pxsize,
 	}
 	return;
 }
+
+void init_gdtidt(void)
+{
+	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) 0X00270000;
+	struct GATE_DESCRIPTOR    *idt = (struct GATE_DESCRIPTOR    *) 0X0026f800;
+	int i;
+	
+	/* initialize GDT */
+	for (i = 0; i < 8192; i++) {
+		set_segmdesc(gdt + i, 0, 0, 0);
+	}
+	set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092); // it represents all the memory that CPU can manage. limit = 4G 
+	set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a); // 512K, from  0x00280000, prepared for bootpack.hrb
+	load_gdtr(0xffff, 0x00270000); // with the help of assembly
+	
+	/* initialize IDT */
+	for (i = 0; i < 256; i++) {
+		set_gatedesc(idt + i, 0, 0, 0);
+	}
+	load_idtr(0x7ff, 0x0026f800); // with the help of assembly
+	
+	return;
+}
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar)
+{
+	if (limit > 0xfffff) {
+		ar |= 0x8000; /* G_bit = 1 */
+		limit /= 0x1000;
+	}
+	sd->limit_low    = limit & 0xffff;
+	sd->base_low     = base & 0xffff;
+	sd->base_mid     = (base >> 16) & 0xff;
+	sd->access_right = ar & 0xff;
+	sd->limit_high   = ((limit >> 16) & 0x0f)|((ar >> 8) & 0xf0);
+	sd->base_high    = (base >> 24) & 0xff;
+	return;
+ }
+ 
+ void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar)
+ {
+	gd->offset_low   = offset & 0xffff;
+	gd->selector     = selector;
+	gd->dw_count     = (ar >> 8) & 0xff;
+	gd->access_right = ar & 0xff;
+	gd->offset_high  = (offset >> 16) & 0xffff;
+	return;
+ }
--- tolset/harib01b/naskfunc.nas
+++ tolset/harib01b/naskfunc.nas
@@ -10,6 +10,7 @@
 		GLOBAL	_io_in8, _io_in16, _io_in32
 		GLOBAL	_io_out8, _io_out16, _io_out32
 		GLOBAL	_io_load_eflags, _io_store_eflags
+		GLOBAL	_load_gdtr, _load_idtr
 		
 [SECTION .text]
 
@@ -76,7 +77,17 @@ _io_store_eflags:	; void io_store_eflags(int eflags);
 		POPFD		; pop data(from EAX) into EFLAGS
 		RET
 		
-		
+_load_gdtr:		; void load_gdtr(int limit, int addr);
+		MOV		AX,[ESP+4]		; limit
+		MOV		[ESP+6],AX
+		LGDT	[ESP+6]
+		RET
+
+_load_idtr:		; void load_idtr(int limit, int addr);
+		MOV		AX,[ESP+4]		; limit
+		MOV		[ESP+6],AX
+		LIDT	[ESP+6]
+		RET		
 		
 		
 		
