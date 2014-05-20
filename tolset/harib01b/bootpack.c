a28f46712cfb2017bc4ab5d576bf281eb321a8f1
bluecloudmatrix <bluecloudmatrix@gmail.com>
2014-05-20 16:20:27 +0800

    day04

--- tolset/harib01b/bootpack.c
+++ tolset/harib01b/bootpack.c
@@ -1,16 +1,76 @@
 void io_hlt(void);
-void write_mem8(int addr, int data);
+void io_cli(void);
+void io_out8(int port, int data);
+//void write_mem8(int addr, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
 
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
 
 void HariMain(void)
 {
 	int i; /* i is an 32-bit integer */
-
-	for (i = 0xa0000; i <= 0xaffff; i++) {
-		write_mem8(i, i & 0x0f);
+	char *p; /* used for BYTE address */
+	
+	init_palette(); /* setting palette */
+	
+	p = (char *) 0xa0000; /* address */
+	
+	for (i = 0; i <= 0xffff; i++) {
+		//write_mem8(i, i & 0x0f);
+		//*i = i & 0x0f; // MOV [i], (i & 0x0f) // We do not know [i] is  BYTE, WORD or DWORD, so it is wrong.
+		p[i] = i & 0x0f;
 	}
 
 	for (;;) {
 		io_hlt();
 	}
+}
+
+void init_palette(void)
+{
+	/* char a[3]; 
+	
+	a:
+		RESB 3
+	
+	*/
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,   /* 0:black */
+		0xff, 0x00, 0x00,	/*  1:bright red */
+		0x00, 0xff, 0x00,	/*  2:bright green */
+		0xff, 0xff, 0x00,	/*  3:bright yellow */
+		0x00, 0x00, 0xff,	/*  4:bright blue */
+		0xff, 0x00, 0xff,	/*  5:bright purple */
+		0x00, 0xff, 0xff,	/*  6:shallow bright blue */
+		0xff, 0xff, 0xff,	/*  7:white */
+		0xc6, 0xc6, 0xc6,	/*  8:bright grey */
+		0x84, 0x00, 0x00,	/*  9:dark red */
+		0x00, 0x84, 0x00,	/* 10:dark green */
+		0x84, 0x84, 0x00,	/* 11:dark yellow */
+		0x00, 0x00, 0x84,	/* 12:dark blue */
+		0x84, 0x00, 0x84,	/* 13:dark purple */
+		0x00, 0x84, 0x84,	/* 14:shallow dark blue */
+		0x84, 0x84, 0x84	/* 15:dark grey */
+	};
+	
+	set_palette(0, 15, table_rgb);
+	return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = io_load_eflags(); /* record the value of interrupt enable */
+	io_cli(); /* set eflags to 0, disable interrupt */
+	io_out8(0x03c8, start); /* send data to specific device */ /* 0x03c8 represents the palette index register, used for write */
+	for(i = start; i <= end; i++) {
+		io_out8(0x03c9, rgb[0] / 4); /* 0x03c8 represents the palette data register, saving red, green and blue */
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags); /* recover eflags */
+	return;
 }
--- tolset/harib01b/naskfunc.nas
+++ tolset/harib01b/naskfunc.nas
@@ -14,8 +14,8 @@ _io_hlt:	; void io_hlt(void);
 		HLT
 		RET
 
-_write_mem8:	; void write_mem8(int addr, int data);
-		MOV		ECX,[ESP+4]		; [ESP+4]saves address��and read it into ECX
-		MOV		AL,[ESP+8]		; [ESP+8]saves data��and read it into AL
-		MOV		[ECX],AL
-		RET
+;_write_mem8:	; void write_mem8(int addr, int data);
+;		MOV		ECX,[ESP+4]		; [ESP+4]saves address��and read it into ECX
+;		MOV		AL,[ESP+8]		; [ESP+8]saves data��and read it into AL
+;		MOV		[ECX],AL
+;		RET
