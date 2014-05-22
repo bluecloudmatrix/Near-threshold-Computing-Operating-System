10686268af0064fca0600d984cb45799b34b7772
bluecloudmatrix <bluecloudmatrix@gmail.com>
2014-05-22 10:29:57 +0800

    mouse_move_harib05d

--- tolset/harib01b/bootpack.c
+++ tolset/harib01b/bootpack.c
@@ -9,20 +9,25 @@
 #define KBC_MODE				0x47
 #define KEYCMD_SENDTO_MOUSE		0xd4
 #define MOUSECMD_ENABLE			0xf4
-void wait_KBC_sendready(void);
 void init_keyboard(void);
-void enable_mouse(void);
 
 extern struct FIFO8 keyfifo;
 extern struct FIFO8 mousefifo;
+
+struct MOUSE_DEC {
+	unsigned char buf[3], phase;
+	int x, y, btn;
+};
+void enable_mouse(struct MOUSE_DEC *mdec);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
-	unsigned char mouse_dbuf[3], mouse_phase;
 	int mx, my, i;
+	struct MOUSE_DEC mdec;
 	
 	mx = (binfo->scrnx - 16) / 2;  
 	my = (binfo->scrny - 28 - 16) / 2;
@@ -38,7 +43,7 @@ void HariMain(void)
 	fifo8_init(&keyfifo, 32, keybuf);
 	fifo8_init(&mousefifo, 128, mousebuf);
 	
-	io_out8(PIC0_IMR, 0xf9); /* PIC1 (11111001) */
+	io_out8(PIC0_IMR, 0xf9); /* (11111001) */
 	io_out8(PIC1_IMR, 0xef); /* (11101111) */
 	
 	init_palette(); /* setting palette */
@@ -50,8 +55,7 @@ void HariMain(void)
 	//sprintf(s, "scrnx = %d", binfo->scrnx);	
 	//putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
 
-	enable_mouse();
-	mouse_phase = 0; // enter the 0xfa state: waiting for mouse
+	enable_mouse(&mdec);
 	
 	for (;;) {
 		//io_hlt();
@@ -74,26 +78,41 @@ void HariMain(void)
 				
 				i = fifo8_get(&mousefifo);
 				io_sti();
-				if(mouse_phase == 0) {
-					// waiting for mouse
-					if (i == 0xfa) {
-						mouse_phase = 1;
+				if (mouse_decode(&mdec, i) != 0) {
+				
+					sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+					if ((mdec.btn & 0x01) != 0) {
+						s[1] = 'L';
 					}
-				} else if (mouse_phase == 1) {
-					// first byte from mouse
-					mouse_dbuf[0] = i;
-					mouse_phase = 2;
-				} else if (mouse_phase == 2) {
-					// second byte from mouse
-					mouse_dbuf[1] = i;
-					mouse_phase = 3;
-				} else if (mouse_phase == 3) {
-					mouse_dbuf[2] = i;
-					mouse_phase = 1;
-					
-					sprintf(s, "%02X %02X %02X", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
-					boxfill8(binfo->vram, binfo->scrnx, COL8_0000FF, 32, 16, 32 + 8 * 8 -1, 31);
+					if ((mdec.btn & 0x02) != 0) {
+						s[3] = 'R';
+					}
+					if ((mdec.btn & 0x04) != 0) {
+						s[2] = 'C';
+					}
+					boxfill8(binfo->vram, binfo->scrnx, COL8_0000FF, 32, 16, 32 + 15 * 8 - 1, 31);
 					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+					
+					// move mouse cursor
+					boxfill8(binfo->vram, binfo->scrnx, COL8_0000FF, mx, my, mx + 15, my + 15); // hide mouse
+					mx += mdec.x;
+					my += mdec.y;
+					if (mx < 0) {
+						mx = 0;
+					}
+					if (my < 0) {
+						my = 0;
+					}
+					if (mx > binfo->scrnx - 16) {
+						mx = binfo->scrnx - 16;
+					}
+					if (my > binfo->scrny - 16) {
+						my = binfo->scrny - 16;
+					}
+					sprintf(s, "(%3d, %3d)", mx, my);
+					boxfill8(binfo->vram, binfo->scrnx, COL8_0000FF, 0, 0, 79, 15); // hide coordinate
+					putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s); // show coordinate
+					putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16); // draw mouse
 				}
 			}
 		}
@@ -121,12 +140,57 @@ void init_keyboard(void)
 	return;
 }
 
-void enable_mouse(void)
+void enable_mouse(struct MOUSE_DEC *mdec)
 {
 	/* activate mouse */
 	wait_KBC_sendready();
 	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
 	wait_KBC_sendready();
 	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+	
+	mdec->phase = 0; // waiting for mouse
 	return;
+}
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
+{
+	if (mdec->phase == 0) {
+		// waiting for mouse
+		if (dat == 0xfa) {
+			mdec->phase = 1;
+		}
+		return 0;
+	}
+	if (mdec->phase == 1) {
+		// first byte from mouse
+		if ((dat & 0xc8) == 0x08) {
+			/* if right */
+			mdec->buf[0] = dat;
+			mdec->phase = 2;
+		}
+		return 0;
+	}
+	if (mdec->phase == 2) {
+		// second byte from mouse
+		mdec->buf[1] = dat;
+		mdec->phase = 3;
+		return 0;
+	}
+	if (mdec->phase == 3) {
+		// third byte from mouse
+		mdec->buf[2] = dat;
+		mdec->phase = 1;
+		mdec->btn = mdec->buf[0] & 0x07;
+		mdec->x = mdec->buf[1];
+		mdec->y = mdec->buf[2];
+		if ((mdec->buf[0] & 0x10) != 0) {
+			mdec->x |= 0xffffff00;
+		}
+		if ((mdec->buf[0] & 0x20) != 0) {
+			mdec->y |= 0xffffff00;
+		}
+		mdec->y = - mdec->y; /* the direction of y is opposite to the screen */
+		return 1;
+	}
+	return -1; /* It should not come here */
 }
