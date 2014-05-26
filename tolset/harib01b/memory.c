b0d9c13fa3867c59ccd2c8c3c824318d76184a6c
bluecloudmatrix <bluecloudmatrix@gmail.com>
2014-05-26 21:10:02 +0800

    memory_4k_harib07a

--- tolset/harib01b/memory.c
+++ tolset/harib01b/memory.c
@@ -185,3 +185,20 @@ int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size)
 	man->lostsize += size;
 	return -1;
 }
+
+// 4K
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size)
+{
+	unsigned int a;
+	size = (size + 0xfff) & 0xfffff000;
+	a = memman_alloc(man, size);
+	return a;
+}
+
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size)
+{
+	int i;
+	size = (size + 0xfff) & 0xfffff000;
+	i = memman_free(man, addr, size);
+	return i;
+}
